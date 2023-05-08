b2398ddc94a0552740ab1411b23a2e90bbf5851f
张宪鹏 <dlhx_zhangxp@yonyou.com>
2023-05-08 15:33:39 +0800

    1.完善登陆系统

--- reggie_take_out/src/main/java/org/example/Main.java
+++ reggie_take_out/src/main/java/org/example/Main.java
@@ -3,11 +3,13 @@ package org.example;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
 @Slf4j
+@ServletComponentScan
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
